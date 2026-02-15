2358bddf412728522fa05de452fef8dce9c605eb
TapasPulis <rooneysamragya10@gmail.com>
2026-02-15 16:52:34 +0100

    Connected back end and front end using CORS

--- src/app.ts/app.ts
+++ src/app.ts/app.ts
@@ -5,10 +5,17 @@ import { errorHandler } from "../middleware/error.middleware";
 import authRoutes from "../routes/auth.routes";
 import deckRoutes from "../routes/deck.routes";
 import flashcardRoutes from "../routes/flashcard.routes";
-
+import cors from "cors";
 export const createApp = () => {
   const app = express();
   app.use(express.json());
+
+  app.use(
+    cors({
+      origin: "http://localhost:5173", // Update this to match your frontend URL
+      credentials: true, // Allow cookies to be sent with requests
+    }),
+  );
 
   app.use("/api/notes", notesRoutes);
 
